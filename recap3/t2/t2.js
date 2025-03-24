d028ba4a1c0699a0e3cbcdc3eb3152ec92ad06b3
aruraruri <aurilait@gmail.com>
2025-03-24 18:53:31 +0200

    Add t2 todo list assignment using DOM functions

--- recap3/t2/t2.js
+++ recap3/t2/t2.js
@@ -2,29 +2,49 @@
 const todoList = [
   {
     id: 1,
-    task: 'Learn HTML',
+    task: "Learn HTML",
     completed: true,
   },
   {
     id: 2,
-    task: 'Learn CSS',
+    task: "Learn CSS",
     completed: true,
   },
   {
     id: 3,
-    task: 'Learn JS',
+    task: "Learn JS",
     completed: false,
   },
   {
     id: 4,
-    task: 'Learn TypeScript',
+    task: "Learn TypeScript",
     completed: false,
   },
   {
     id: 5,
-    task: 'Learn React',
+    task: "Learn React",
     completed: false,
   },
 ];
 
 // add your code here
+
+const ul = document.querySelector("ul");
+
+todoList.forEach((todo) => {
+  const li = document.createElement("li");
+
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = `todo${todo.id}`;
+  input.checked = todo.completed ? "checked" : "";
+
+  const label = document.createElement("label");
+  label.htmlFor = `todo${todo.id}`;
+  label.innerText = todo.task;
+
+  li.append(input, label);
+
+  ul.appendChild(li);
+  navigator.
+});
