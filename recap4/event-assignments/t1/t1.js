f37d94272aae14ba1eecb10c9246793ef84e2247
aruraruri <aurilait@gmail.com>
2025-03-25 12:03:09 +0200

    Non working almost working implementation of todo list item deletion

--- recap4/event-assignments/t1/t1.js
+++ recap4/event-assignments/t1/t1.js
@@ -1,5 +1,5 @@
 // array for todo list
-const todoList = [
+let todoList = [
   {
     id: 1,
     task: 'Learn HTML',
@@ -28,3 +28,54 @@ const todoList = [
 ];
 
 // add your code here
+
+const ul = document.querySelector('ul');
+
+todoList.forEach(todo => {
+  const li = document.createElement('li');
+
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = `todo${todo.id}`;
+  input.checked = todo.completed ? 'checked' : '';
+
+  const label = document.createElement('label');
+  label.htmlFor = `todo${todo.id}`;
+  label.innerText = todo.task;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'x';
+
+  li.append(input, label, deleteButton);
+
+  ul.appendChild(li);
+});
+
+function getListItems() {
+  return ul.getElementsByTagName('li');
+}
+
+for (let i = 0; i < getListItems().length; i++) {
+  try {
+    getListItems()
+      [i].getElementsByTagName('input')[0]
+      .addEventListener('click', function () {
+        todoList[i].completed = !todoList[i].completed;
+        console.log(todoList);
+      });
+  } catch (error) {
+    console.warn(error);
+  }
+
+  try {
+    getListItems()
+      [i].getElementsByTagName('button')[0]
+      .addEventListener('click', function () {
+        console.log('AA');
+        todoList = todoList.splice(i, 1);
+        getListItems()[i].remove();
+      });
+  } catch (error) {
+    console.warn(error);
+  }
+}
