56216ff835b4a4857d74bc7baaabd863e10c5a8f
aruraruri <aurilait@gmail.com>
2025-03-25 09:30:11 +0200

    Add t3.js script to display browser and screen information in t3.html

---
+++ recap3/t3/t3.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const targetDiv = document.querySelector("#target");
+
+const p1 = document.createElement("p");
+p1.innerText = `Your browser is: ${navigator.userAgent}`;
+
+const p2 = document.createElement("p");
+p2.innerText = `Screen width and height is: ${window.screen.width}px wide/${window.screen.height}px high`;
+
+const p3 = document.createElement("p");
+p3.innerText = `Document size is: ${window.innerWidth}px wide/${window.innerHeight}px high`;
+
+const currentDate = new Date();
+const timestamp = currentDate.getTime();
+const p4 = document.createElement("p");
+p4.innerText = `Current date and time is: ${currentDate.toLocaleDateString()}, ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+
+targetDiv.append(p1, p2, p3, p4);
